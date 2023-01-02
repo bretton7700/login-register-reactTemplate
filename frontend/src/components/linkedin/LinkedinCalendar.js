90ee4d64c3974133ee6e82a7706bfa84391d8b29
root <gitongabretton@gmail.com>
2023-01-02 11:09:17 +0000

    calendar populated

--- frontend/src/components/linkedin/LinkedinCalendar.js
+++ frontend/src/components/linkedin/LinkedinCalendar.js
@@ -16,64 +16,60 @@ const LinkedinCalendar = () => {
   const navigate = useNavigate();
   const location = useLocation();
   
+  const getEvents = async (email) => {
+    try {
+      const response = await axiosPrivate.get(`/posts/${email}`, {
+        signal: controller.signal
+      });
+
+      // map the event data to the expected format
+      const calendarEvents = response.data.map(event => ({
+        start: moment(event.scheduledTime).toDate(),
+        // set the end date to the start date + 1 hour
+        end: moment(event.scheduledTime).add(1, 'hour').toDate(),
+        title: event.description
+      }));
+
+      // update the events state variable
+      setEvents(calendarEvents);
+    } catch (err) {
+      console.error(err);
+      navigate('/login', { state: { from: location }, replace: true });
+    }
+  };
 
   useEffect(() => {
     let isMounted = true;
     const controller = new AbortController();
 
-    const getEvents= async (email) => {
-        try {
-            const response = await axiosPrivate.get(`/posts/${email}`, {
-                signal: controller.signal
-            });
+    const fetchData = async () => {
+      if (isMounted) {
+        await getEvents(userEmail);
+      }
+    };
 
-
-            isMounted && setEvents(response.data);
-
-
-
-        } catch (err) {
-            console.error(err);
-            navigate('/login', { state: { from: location }, replace: true });
-        }
-    }
-
-    getEvents(userEmail);
-
+    fetchData();
 
     return () => {
-        isMounted = false;
-        controller.abort();
-    }
-}, [userEmail])
-
-
- 
+      isMounted = false;
+      controller.abort();
+    };
+  }, [userEmail]);
 
   return (
     <>
-     
-
       <Card className="border-ndovu rounded-0 my-3" style={{ maxWidth: '900px', width: '100%' }}>
         <Card.Header>Calendar</Card.Header>
         <Card.Body>
-
           <Calendar
             localizer={localizer}
             events={events}
             startAccessor="start"
             endAccessor="end"
           />
-
         </Card.Body>
       </Card>
-
-
-
-
-
     </>
-
   );
 };
 
