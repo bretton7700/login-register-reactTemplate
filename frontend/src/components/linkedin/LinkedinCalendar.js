34f3eb1fdc7e24d81977815cd53d5dc091b8cadb
root <gitongabretton@gmail.com>
2023-01-02 10:24:55 +0000

    get posts

--- frontend/src/components/linkedin/LinkedinCalendar.js
+++ frontend/src/components/linkedin/LinkedinCalendar.js
@@ -1,14 +1,51 @@
-import React, { useState } from 'react';
+import React, { useEffect,useState } from 'react';
 import { Calendar, momentLocalizer } from 'react-big-calendar';
 import moment from 'moment';
 import {  Card } from "react-bootstrap";
 import "react-datepicker/dist/react-datepicker.css";
-
+import useAxiosPrivate from "../../hooks/useAxiosPrivate";
 import 'react-big-calendar/lib/css/react-big-calendar.css';
+import { useLocation, useNavigate } from "react-router-dom";
+
 const localizer = momentLocalizer(moment);
+const userEmail = localStorage.getItem('userEmail');
 
 const LinkedinCalendar = () => {
   const [events, setEvents] = useState([]); // list of scheduled posts
+  const axiosPrivate = useAxiosPrivate();
+  const navigate = useNavigate();
+  const location = useLocation();
+  
+
+  useEffect(() => {
+    let isMounted = true;
+    const controller = new AbortController();
+
+    const getEvents= async (email) => {
+        try {
+            const response = await axiosPrivate.get(`/posts/${email}`, {
+                signal: controller.signal
+            });
+
+
+            isMounted && setEvents(response.data);
+
+
+
+        } catch (err) {
+            console.error(err);
+            navigate('/login', { state: { from: location }, replace: true });
+        }
+    }
+
+    getEvents(userEmail);
+
+
+    return () => {
+        isMounted = false;
+        controller.abort();
+    }
+}, [userEmail])
 
 
  
