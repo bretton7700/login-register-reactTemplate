b4752057ebbb900bf033e0a0b65769aa9322bfdb
root <gitongabretton@gmail.com>
2023-01-02 07:11:57 +0000

    styling big calendar

--- frontend/src/components/linkedin/LinkedinCalendar.js
+++ frontend/src/components/linkedin/LinkedinCalendar.js
@@ -2,12 +2,14 @@ import React, { useState } from 'react';
 import { Calendar, momentLocalizer } from 'react-big-calendar';
 import DatePicker from 'react-datepicker';
 import moment from 'moment';
+import { Button, Card, Col, Form, Row } from "react-bootstrap";
+import "react-datepicker/dist/react-datepicker.css";
 
 const localizer = momentLocalizer(moment);
 
 const LinkedinCalendar = () => {
   const [events, setEvents] = useState([]); // list of scheduled posts
-  const [postText, setPostText] = useState(''); // content of new post
+  const [description, setDescription] = useState(''); // content of new post
   const [postDate, setPostDate] = useState(null); // date and time for new post
 
   const handleSubmit = () => {
@@ -16,27 +18,69 @@ const LinkedinCalendar = () => {
   };
 
   return (
-    <div>
-      <form onSubmit={handleSubmit}>
-        <textarea value={postText} onChange={e => setPostText(e.target.value)} />
-        <DatePicker
-          selected={postDate}
-          onChange={date => setPostDate(date)}
-          showTimeSelect
-          timeFormat="HH:mm"
-          timeIntervals={15}
-          dateFormat="MMMM d, yyyy h:mm aa"
-          timeCaption="time"
-        />
-        <button type="submit">Schedule post</button>
-      </form>
-      <Calendar
-        localizer={localizer}
-        events={events}
-        startAccessor="start"
-        endAccessor="end"
-      />
-    </div>
+    <>
+      <Card className="border-ndovu rounded-0 my-3" style={{ maxWidth: '900px', width: '100%' }}>
+        <Card.Header>Schedule Post</Card.Header>
+        <Card.Body>
+          <Form >
+            <Row>
+
+
+              <Col sm={6}>
+
+                <Form.Group className="mb-3">
+                  <Form.Label>Text</Form.Label>
+                  <Form.Control type="text" placeholder="Enter Your Description" name="rootPassword" value={description} required onChange={(e) => { setDescription(e.target.value); }} />
+                  <Form.Control.Feedback type="invalid">Text  required</Form.Control.Feedback>
+                </Form.Group>
+              </Col>
+
+              <Col sm={6}>
+                <Form.Group className='mb-3'>
+                  <Form.Label>Date</Form.Label>
+                  <DatePicker
+                    selected={postDate}
+                    onChange={date => setPostDate(date)}
+                    showTimeSelect
+                    timeFormat="HH:mm"
+                    timeIntervals={15}
+                    dateFormat="MMMM d, yyyy h:mm aa"
+                    timeCaption="time"
+                  />
+                </Form.Group>
+
+              </Col>
+            </Row>
+
+
+            <Button variant="primary" size="sm" type="submit" id='share' onClick={handleSubmit} >Schedule Post</Button>{" "}
+          </Form>
+
+
+
+        </Card.Body>
+      </Card>
+
+      <Card className="border-ndovu rounded-0 my-3" style={{ maxWidth: '900px', width: '100%' }}>
+        <Card.Header>Schedule Post</Card.Header>
+        <Card.Body>
+
+          <Calendar
+            localizer={localizer}
+            events={events}
+            startAccessor="start"
+            endAccessor="end"
+          />
+
+        </Card.Body>
+      </Card>
+
+
+
+
+
+    </>
+
   );
 };
 
