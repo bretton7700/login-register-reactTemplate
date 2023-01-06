c3d139690327a8e557eced629033a822b7caf7b6
root <gitongabretton@gmail.com>
2023-01-06 10:14:20 +0000

    test image array

--- backend/controllers/linkedinPublish.js
+++ backend/controllers/linkedinPublish.js
@@ -1,35 +1,71 @@
 const linkedinController = require('../controllers/linkedinController');
 
-const publish = async (req,res) =>{
-    const {  description } = req.body;
-    console.log(req.body.description);
-    console.log('above is the descr below userid')
+// const publish = async (req,res) =>{
+//     const {  description } = req.body;
+//     console.log(req.body.description);
+//     console.log('above is the descr below userid')
     
    
 
-    if (!Boolean(description) ) {
-        console.log('one of the values is empty')
-    } else {
-        const content = {
+//     if (!Boolean(description) ) {
+//         console.log('one of the values is empty')
+//     } else {
+//         const content = {
            
-            description: description,
+//             description: description,
             
             
-        };
+//         };
 
-        try {
-            const response = await linkedinController.publishContent(req, content);
-            console.log('.......sending post.....')
-            console.log('post sent')
-            console.log(response)
-            res.status(201).json({ 'success': `New post Published  ` });
+//         try {
+//             const response = await linkedinController.publishContent(req, content);
+//             console.log('.......sending post.....')
+//             console.log('post sent')
+//             console.log(response)
+//             res.status(201).json({ 'success': `New post Published  ` });
 
-        } catch (err) {
-            console.log('.......post not sent.....')
-            console.error(err);
+//         } catch (err) {
+//             console.log('.......post not sent.....')
+//             console.error(err);
 
-        }
-    }
-}
+//         }
+//     }
+// }
+
+const publish = async (req,res) =>{
+
+    const { description, image } = req.body;
+
+if (Boolean(description) && !Boolean(image) ) {
+  // only description
+  const content = {
+    description: description,
+  };
+  try {
+    const response = await linkedinController.publishContent(req, content);
+    console.log('.......sending post.....')
+    console.log('post sent')
+    console.log(response)
+    res.status(201).json({ 'success': `New post Published  ` });
+  } catch (err) {
+    console.log('.......post not sent.....')
+    console.error(err);
+  }
+} else if (Boolean(description) && Boolean(image) ) {
+  // description and image
+  const content = {
+    description: description,
+    video: {
+      originalUrl: image,
+    },
+  };
+  try {
+    console.log('......this is the image array.....')
+    console.log(image)
+  } catch (err) {
+    console.log('.......post not sent.....')
+    console.error(err);
+  }
+}}
 
 module.exports ={ publish }
